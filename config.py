2bc24e0030d53d5607a2bd3c6141e63cb1a863f9
Jurek362 <Jurek.lenard@gmail.com>
2025-05-28 12:03:00 +0200

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    # Ustaw parametry tajne i połączenie do bazy danych
-    SECRET_KEY = 'a-very-secret-key-12345'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/ngl_clone_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
